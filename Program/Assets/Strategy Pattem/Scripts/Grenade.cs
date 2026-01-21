8e6494e8ff99cd5a1629baf871cf1c1b43da91f7
lollonoa <ehdrjs0211@naver.com>
2026-01-21 19:19:40 +0900

    2026.01.19. 경량패턴

--- Program/Assets/Strategy Pattem/Scripts/Grenade.cs
+++ Program/Assets/Strategy Pattem/Scripts/Grenade.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Grenade : Weapon
 {
+    [SerializeField] float throwPower;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Vector3 startPosition;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        startPosition = rigidbody.position;
+    }
     public override void Attack()
     {
-        Debug.Log("Grenade");
+        if(rigidbody.useGravity == false)
+        {
+            rigidbody.useGravity = true;
+
+            rigidbody.AddForce(direction.normalized*throwPower,ForceMode.Impulse);
+        }
+    }
+
+    //OnBecameInvisible 어느 카메라에서든 랜더러가 더 이상 표시되지 않는경우 호출됨
+    private void OnBecameInvisible()
+    {
+        rigidbody.useGravity = false;
+        rigidbody.position = startPosition;
+        rigidbody.linearVelocity = Vector3.zero;
     }
 
 }
