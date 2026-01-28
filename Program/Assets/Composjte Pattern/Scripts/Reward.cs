ef2cb15c8c463253ec4c0ebd1b6548c41f01d8fb
lollonoa <ehdrjs0211@naver.com>
2026-01-29 01:12:04 +0900

    2026.01.23 복합체 패턴

---
+++ Program/Assets/Composjte Pattern/Scripts/Reward.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reward : MonoBehaviour
+{
+    [SerializeField] int random;
+    [SerializeField] Data [] dataList;
+    [SerializeField] Image image;
+    
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    void Start()
+    {
+        random=Random.Range(0, dataList.Length);
+
+        gameObject.name = dataList[random].Name;
+        image.sprite = dataList[random].GetSprite;
+    }
+
+}
